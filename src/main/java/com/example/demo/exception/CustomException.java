cd06469ccd6cb7db84a15888758fc93e33286cc3
wjddnr <wjddnr1011@gmail.com>
2025-02-11 23:14:03 +0900

    feat : 예외처리 중앙화를 위한 Custom Exception 및 ExceptionType enum 생성

---
+++ src/main/java/com/example/demo/exception/CustomException.java
@@ -0,0 +1,18 @@
+package com.example.demo.exception;
+
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException {
+    private ExceptionType type;
+
+    public CustomException(ExceptionType type) {
+        super();
+    }
+
+    public CustomException(ExceptionType type, Object message) {
+        super(type.getDesc() + message.toString());
+        this.type = type;
+    }
+
+}
